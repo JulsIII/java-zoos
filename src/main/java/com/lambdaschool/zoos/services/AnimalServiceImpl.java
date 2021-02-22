11dd459b256010a0bd09f60c9d22bc1e416f9465
Your Name <you@example.com>
2021-02-21 22:15:57 -0600

    10 - SMall fixes

--- src/main/java/com/lambdaschool/zoos/services/AnimalServiceImpl.java
+++ src/main/java/com/lambdaschool/zoos/services/AnimalServiceImpl.java
@@ -1,7 +1,29 @@
 package com.lambdaschool.zoos.services;
 
+import com.lambdaschool.zoos.models.Animal;
+import com.lambdaschool.zoos.models.Zoo;
+import com.lambdaschool.zoos.repositories.AnimalRepository;
+import com.lambdaschool.zoos.repositories.ZooRepository;
+import com.lambdaschool.zoos.views.CountAnimalInZoo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AnimalServiceImpl
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+
+@Service(value = "zooService")
+public class AnimalServiceImpl implements AnimalService
 {
+    @Autowired
+    private AnimalRepository animalrepos;
+
+
+    @Override
+    public ArrayList<CountAnimalInZoo> getCountAnimalInZoo()
+    {
+        ArrayList<CountAnimalInZoo> list = new ArrayList<>();
+        animalrepos.findAll();
+        return list;
+    }
 
 }
